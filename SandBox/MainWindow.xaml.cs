14254fe8f0d1abb440687d6b10c2ddcb4676d86f
castor <castor@naraspace.com>
2021-08-04 16:30:51 +0900

    1. Call 방식 처리시간 Test 2. Value Type Double / String 비교 Test

--- SandBox/MainWindow.xaml.cs
+++ SandBox/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
@@ -52,6 +54,169 @@ namespace SandBox
 
             Debug.WriteLine(myClass.ParentData.Name);
         }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CallValueTypeCompareMethod();
+
+        }
+
+        private void CallTypeTestMethod()
+        {
+            List<TestClass> test = new List<TestClass>();
+
+            for (int i = 0; i < 700; i++)
+            {
+                TestClass tempclass = new TestClass() { Name = string.Format("Test_{0:D3}", i) };
+
+                test.Add(tempclass);
+
+                callMeEvent += tempclass.callMe;
+            }
+
+            string findStr = "Test_350";
+            string findStrLow = findStr.ToLower();
+
+            #region MyRegion
+            Stopwatch st = new Stopwatch();
+            st.Reset();
+            st.Start();
+            #endregion
+
+            Queue<TestClass> tempQueue = new Queue<TestClass>();
+            callMeEvent(tempQueue, findStr);
+
+            #region test
+            st.Stop();
+            Console.Write("EventCall {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+
+            st.Reset();
+            st.Start();
+            #endregion
+
+            TestClass temp1 = test.Find(x => x.Name.Equals(findStr));
+
+            #region test
+            st.Stop();
+            Console.Write("Lamda {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+
+            st.Reset();
+            st.Start();
+            #endregion
+
+            TestClass temp2 = test.Find(x => x.Name.ToLower().Equals(findStrLow));
+
+            #region test
+            st.Stop();
+            Console.Write("Lamda Lower {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+
+            st.Reset();
+            st.Start();
+            #endregion
+
+            var temp3 =
+                (from testitem in test
+                 where testitem.Name == findStr
+                 select testitem).FirstOrDefault();
+
+            #region test
+            st.Stop();
+            Console.Write("LINQ {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+
+            st.Reset();
+            st.Start();
+            #endregion
+
+            var temp4 =
+               (from testitem in test
+                where testitem.Name.ToLower() == findStrLow
+                select testitem).FirstOrDefault();
+
+            #region test
+            st.Stop();
+            Console.WriteLine("LINQ tolower {0} [{1}]", st.ElapsedMilliseconds, st.ElapsedTicks);
+            #endregion
+
+        }
+
+        public delegate void CallMeDelegate(Queue<TestClass> que, string name);
+        public event CallMeDelegate callMeEvent;
+
+        private void CallValueTypeCompareMethod()
+        {
+            List<TestClass> test = new List<TestClass>();
+
+            for (int i = 0; i < 700; i++)
+            {
+                TestClass tempclass = new TestClass() { Name = string.Format("Test_{0:D3}", i), testValue1 = i.ToString(), testValue2 = i };
+
+                test.Add(tempclass);
+            }
+
+            Stopwatch st = new Stopwatch();
+            st.Reset();
+            st.Start();
+
+            double sum = 0.0;
+
+            foreach (var item in test)
+            {
+                sum += double.Parse(item.testValue1);
+            }
+
+            st.Stop();
+            Console.WriteLine("Test {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+            st.Reset();
+            st.Start();
+
+            double sum2 = 0.0;
+
+            foreach (var item in test)
+            {
+                sum2 += item.testValue2;
+            }
+
+            st.Stop();
+            Console.WriteLine("Test {0} [{1}]\t", st.ElapsedMilliseconds, st.ElapsedTicks);
+        }
+    }
+
+    public class TestClass
+    {
+        private string _name = "";
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+
+                }
+            }
+        }
+
+        public int value { get; set; } = 0;
+
+        public string testValue1 { get; set; } = "0.0";
+
+        public double testValue2 { get; set; } = 0.0;
+
+
+
+        public void callMe(Queue<TestClass> que, string name)
+        {
+            if(Name.Equals(name))
+            {
+                que.Enqueue(this);
+
+                value = 1;
+            }
+        }
+
+
     }
 
     public interface ITreeViewParentLink
